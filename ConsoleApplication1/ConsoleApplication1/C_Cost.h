3e79f056a8a3ed17944e9b8808be2829bc8c35eb
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-07-20 19:35:38 +0300

    add: cost

---
+++ ConsoleApplication1/ConsoleApplication1/C_Cost.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+
+namespace cost
+{
+	// расстояние * расход топлива 100 км * стоимость топлива / 100 = расхода топлива
+	class C_Calculator
+	{
+	public:
+		C_Calculator()
+		{
+			
+		}
+		C_Calculator()
+		{
+
+		}
+	};
+private:
+	float 
+}
